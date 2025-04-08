715f6b9bb3a0a4a1fbc39a30e09cb107675ba39e
ranchen <ranchen@ucsb.edu>
2025-04-08 16:25:42 -0700

    changed all names to real names

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -16,11 +16,7 @@ public class Developer {
      */
 
     public static String getName() {
-        // TODO: Change this to your name
-        // You may use just the name that is used on <https://bit.ly/cs156-s25-teams>
-        // i.e. your first name, or your first and initial of last name
-
-        return "Chris G.";
+        return "Ran C.";
     }
 
     /**
@@ -29,8 +25,7 @@ public class Developer {
      */
 
     public static String getGithubId() {
-        // TODO: Change this to your github id
-        return "cgaucho";
+        return "rankesterc";
     }
 
     /**
@@ -39,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("s25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("s25-01");
+        team.addMember("Alec S.");
+        team.addMember("Eyob T.");
+        team.addMember("Hao J.");
+        team.addMember("Lexuan H.");
+        team.addMember("Oscar L.");
+        team.addMember("Ran C.");
         return team;
     }
 }
