379fb1a66bdfaacac7fb7ec3b6265e5b97524bb2
ranchen <ranchen@ucsb.edu>
2025-04-08 17:40:26 -0700

    add mutation equal hash test

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -30,6 +30,23 @@ public class DeveloperTest {
         assertEquals("rankesterc", Developer.getGithubId());
     }
 
+    @Test
+    public void getTeam_returns_team_with_correct_name() {
+        Team  t = Developer.getTeam();
+        assertEquals("s25-01", t.getName());
+    }
+
+    @Test
+    public void getTeam_returns_team_with_correct_members() {
+        Team  t = Developer.getTeam();
+        assertTrue(t.getMembers().contains("Alec S."),"Team should contain Alec S.");
+        assertTrue(t.getMembers().contains("Eyob T."),"Team should contain Eyob T.");
+        assertTrue(t.getMembers().contains("Hao J."),"Team should contain Hao J.");
+        assertTrue(t.getMembers().contains("Lexuan H."),"Team should contain Lexuan H.");
+        assertTrue(t.getMembers().contains("Oscar L."),"Team should contain Oscar L.");
+        assertTrue(t.getMembers().contains("Ran C."),"Team should contain Ran C.");
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
